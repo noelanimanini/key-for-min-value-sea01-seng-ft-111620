e0025fb68c40a4c310030503ea901bde19ca4936
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-11 01:08:41 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+min_key = nil
+min_value = nil
+
+name_hash.each do |key, value|
+
+  if min_key == nil
+    min_key = key
+    min_value = value
+  elsif value < min_value
+    min_key = key
+    min_value = value
+  end
+end 
+     min_key
 end
