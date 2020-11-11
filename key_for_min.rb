c17396a89d2635ddbdac0ebbef4f4a2ddbe9c0ae
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-11 01:07:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,17 +4,16 @@
 def key_for_min_value(name_hash)
 
 min_key = nil
-min_value = nil 
+min_value = nil
 
 name_hash.each do |key, value|
 
   if min_key == nil
-    min_key = key 
-    min_value = value 
+    min_key = key
+    min_value = value
   elsif value < min_value
-    min_key = key 
-    min_value = value 
+    min_key = key
+    min_value = value
   end
      min_key
 end
-
